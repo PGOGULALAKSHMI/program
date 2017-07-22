c9a5e4ea27760a2bbbf01b3a82d68737717182fc
PGOGULALAKSHMI <gogulalakshmiece@gmail.com>
2017-07-22 09:50:07 +0530

    Update leapyear.c

--- leapyear.c
+++ leapyear.c
@@ -3,10 +3,13 @@ int main()
 {
 int n;
 scanf("%d",&n);
+  while(n>0)
+  {
 if(n%400==0)
 printf("leap year");
 else if(n%100==0)
 printf("not leap year");
 else if(n%4==0)
 printf("leap year");
+  }
 }
