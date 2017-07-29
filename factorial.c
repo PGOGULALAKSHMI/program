0aaa806263eb196844be1647e151583171b428e0
PGOGULALAKSHMI <gogulalakshmiece@gmail.com>
2017-07-29 06:33:09 -0700

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -5,8 +5,13 @@ int main(void) {
 	// your code goes here
 	int n,i,f=1;
 	scanf("%d",&n);
+	if(n<1)
+		printf("invalid input");
+	else
+	{
 	for(i=1;i<=n;i++)
 	f=f*i;
 	printf("%d",f);
+	}
     return 0;
 }
