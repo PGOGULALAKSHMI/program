1e5b01ab7e0d8552608957e60f4ed46e89032ff6
PGOGULALAKSHMI <gogulalakshmiece@gmail.com>
2017-08-12 16:44:57 +0530

    Update primedisplay.c

--- primedisplay.c
+++ primedisplay.c
@@ -4,6 +4,8 @@ int main(void) {
 	// your code goes here
 	int f,l,i,j,c=0;
 	scanf("%d %d",&f,&l);
+	if((f>1)&&(l>1))
+	{
 	for(i=f;i<=l;i++)
 	{
 		for(j=2;j<i;j++)
@@ -14,5 +16,8 @@ int main(void) {
 		if(c==0)
 		printf(" %d",i);
 	}
+	}
+	else
+		printf("invalid number");
 	return 0;
 }
